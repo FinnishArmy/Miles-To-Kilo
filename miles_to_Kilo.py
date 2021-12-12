6d013e70994b63242cbd60166084e9fa2912b640
FinnishArmy <40931307+FinnishArmy@users.noreply.github.com>
2021-12-11 20:36:48 -0800

    Added the python file.

---
+++ miles_to_Kilo.py
@@ -0,0 +1,11 @@
+def milesToKilo():
+
+    miles = float(input('Enter distance in miles: '))
+
+    conversion_Factor = 1.60934
+
+    kilometers = miles * conversion_Factor
+
+    print('The distance in kilometers is: ', kilometers)
+
+milesToKilo()
